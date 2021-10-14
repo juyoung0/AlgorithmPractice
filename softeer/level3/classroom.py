15a23834296bc854bc2179e984ba6f6cc860baba
Juyoung Oh <ojmme625@gmail.com>
2021-10-14 21:24:26 +0900

    211014: Softeer solved

---
+++ softeer/level3/classroom.py
@@ -0,0 +1,41 @@
+# deque로 하니 시간초과 남. heapq으로 수정함
+# 그래도 시간초과남
+
+import heapq
+
+if __name__=="__main__":
+    n = int(input())
+    heap = []
+    for _ in range(n):
+        # 강의가 끝나는 시간 순으로 정렬하기 위해 heap에 순서를 반대로 넣는다
+        class_time = tuple(map(int, input().split()))
+        heapq.heappush(heap, (class_time[1], class_time[0]))
+
+    time, cnt = 0, 0
+    while heap:
+        item = heapq.heappop(heap)
+        # 강의 시작시간이 현재 시간보다 크거나 같으면 들을 수 있다
+        if item[1] >= time:
+            time = item[0]
+            cnt += 1
+
+    print(cnt)
+
+# from collections import deque
+#
+# if __name__ == "__main__":
+#     n = int(input())
+#     temp = []
+#     for _ in range(n):
+#         temp.append(tuple(map(int, input().split())))
+#     temp.sort(key=lambda x: x[1])
+#     classes = deque(temp)
+#
+#     time, cnt = 0, 0
+#     while classes:
+#         time = classes.popleft()[1]
+#         cnt += 1
+#         while classes and classes[0][0] < time:
+#             classes.popleft()
+#
+#     print(cnt)
