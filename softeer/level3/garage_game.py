baf478f878c080d78a884629d05ea221036d342e
Juyoung Oh <ojmme625@gmail.com>
2021-10-22 00:04:27 +0900

    211021: softeer level3 solved

--- softeer/level3/garage_game.py
+++ softeer/level3/garage_game.py
@@ -1,25 +1,36 @@
+# 시간초과 남
+# 1. 위에서 아래로 내려오는 테트리스 구조이므로 행,열이 아닌 열,행으로 저장함
+# 2. 빈칸 채울떄 하나씩 이동하지 않고, 남아있는 차를 모아서 한번에 이동시킴
+# 3. 뭘 더 해야 할까..?
+
 import sys
 from collections import deque
+import copy
 
 # 인풋 받기
 n = int(sys.stdin.readline())
-waits = []
-garage = []
-answer = 0
-wait_idx = [2*n-1 for _ in range(n)]
+waits = [[0 for _ in range(2*n)] for _ in range(n)]
+garage = [[0 for _ in range(n)] for _ in range(n)]
+max_answer, min_answer = 0, 0
+wait_idx = [2*n for _ in range(n)]
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-for _ in range(2*n):
-    waits.append(list(map(int, sys.stdin.readline().split())))
-for _ in range(n):
-    garage.append(list(map(int, sys.stdin.readline().split())))
+
+for j in range(2*n):
+    data = list(map(int, sys.stdin.readline().split()))
+    for i in range(n):
+        waits[i][j] = data[i]
+for j in range(n):
+    data = list(map(int, sys.stdin.readline().split()))
+    for i in range(n):
+        garage[i][j] = data[i]
 
 # 빈칸을 새로 채우는 함수
 def fill_garage(_garage, removed_num, _wait_idx):
     global n
-    for j in range(n):
-        for i in range(removed_num[j]):
-            _garage[removed_num[j]-i-1][j] = waits[_wait_idx[j]][j]
-            _wait_idx[j] -= 1
+    for i in range(n):
+        _garage[i][:removed_num[i]] = waits[i][_wait_idx[i]-removed_num[i]:_wait_idx[i]]
+        _wait_idx[i] -= removed_num[i]
+
     return _garage, _wait_idx
 
 # 선택된 그룹을 지우는 함수
@@ -32,22 +43,31 @@ def remove_cars(_garage, group, _wait_idx):
         max_x = max(max_x, g[0])
         min_y = min(min_y, g[1])
         max_y = max(max_y, g[1])
-        removed_num[g[1]] += 1
+        removed_num[g[0]] += 1
         _garage[g[0]][g[1]] = -1
 
-    # 현재 있는 차들을 내려서 지운 칸 채우기
-    for j in range(n):
-        for i in range(n-2, -1, -1):
-            k = i
-            if _garage[k][j] != -1:
-                while k+1 < n and _garage[k+1][j] == -1:
-                    _garage[k+1][j] = _garage[k][j]
-                    _garage[k][j] = -1
-                    k += 1
+    # 현재 있는 차들을 내려서 지운 칸 채우기 (버전 1)
+    # for j in range(n):
+    #     for i in range(n-2, -1, -1):
+    #         k = i
+    #         if _garage[k][j] != -1:
+    #             while k+1 < n and _garage[k+1][j] == -1:
+    #                 _garage[k+1][j] = _garage[k][j]
+    #                 _garage[k][j] = -1
+    #                 k += 1
+
+    # 현재 있는 차들을 내려서 지운 칸 채우기 (버전 2)
+    _temp = [[-1 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        idx = n-1
+        for j in range(n-1, -1, -1):
+            if _garage[i][j] != -1:
+                _temp[i][idx] = _garage[i][j]
+                idx -= 1
 
     score += len(group)
     score += (max_x - min_x + 1) * (max_y - min_y + 1)
-    return fill_garage(_garage[:], removed_num, _wait_idx[:]), score
+    return fill_garage(_temp, removed_num, _wait_idx[:]), score
 
 # 다음에 누를 수 있는 버튼 그룹 찾기 (bfs)
 def find_groups(_garage):
@@ -55,8 +75,8 @@ def find_groups(_garage):
     visited = [[0 for _ in range(n)] for _ in range(n)]
     _groups = []
 
-    for i in range(n):
-        for j in range(n):
+    for j in range(n):
+        for i in range(n):
             if visited[i][j] == 1:
                 continue
             group = []
@@ -70,37 +90,27 @@ def find_groups(_garage):
                 curr = pq.popleft()
                 for dir in dirs:
                     next_x, next_y = curr[0] + dir[0], curr[1] + dir[1]
-                    if 0<=next_x<n and 0<=next_y<n and visited[next_x][next_y] == 0 and _garage[next_x][next_y] == color:
-                        #print(next_x, next_y)
-                        group.append((next_x, next_y))
-                        pq.append((next_x, next_y))
-                        visited[next_x][next_y] = 1
+
+                    if 0<=next_x<n and 0<=next_y<n:
+                        if visited[next_x][next_y] == 0 and _garage[next_x][next_y] == color:
+                            group.append((next_x, next_y))
+                            pq.append((next_x, next_y))
+                            visited[next_x][next_y] = 1
             _groups.append(group)
 
     return _groups
 
 # 매 turn 마다 지울수 있는 group 찾아서 지우고 점수 비교하기
 def dfs(_garage, turn, score, _wait_idx):
-    # if turn > 3:
-    #     if score == 30:
-    #         print("Score : ", score)
-    #         print(wait_idx)
-    #         print(garage)
-    #     return score
-    _score = score
-    groups = find_groups(_garage)
-    for group in groups:
-        (_new_garage, _new_wait_idx), _new_score = remove_cars(_garage[:], group, _wait_idx[:])
-        if turn == 3:
-            if score+_new_score == 30:
-                print("Score : ", score)
-                print(wait_idx)
-                print("Old : ", _garage)
-                print("New: ", _new_garage)
-            _score = max(_score, score+_new_score)
-        else:
-            for m in range(n):
-                _score = max(_score, dfs(_new_garage[:], turn+1, score+_new_score, _new_wait_idx[:]))
-    return _score
+    global max_answer, min_answer
+    if turn > 3:
+        min_answer = min(min_answer, score)
+        max_answer = max(max_answer, score)
+    else:
+        groups = find_groups(_garage)
+        for group in groups:
+            (_new_garage, _new_wait_idx), _new_score = remove_cars(copy.deepcopy(_garage), group, _wait_idx[:])
+            dfs(_new_garage, turn+1, score+_new_score, _new_wait_idx[:])
 
-print(dfs(garage[:], 1, 0, wait_idx[:]))
+dfs(garage, 1, 0, wait_idx)
+print(max_answer)
